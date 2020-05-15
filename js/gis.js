7ef22837366cd38ac1177fb8a0a450f2d15c5b39
nicklally <nicklally@gmail.com>
2020-05-15 12:30:58 -0400

    stretch image working

--- js/gis.js
+++ js/gis.js
@@ -19,7 +19,7 @@ new p5();
 
 setup = function() {
   // create canvas
-
+  noLoop();
   var c = createCanvas(document.getElementById("leftCanv").offsetWidth,document.getElementById("leftCanv").offsetHeight, WEBGL);
 
   c.parent("leftCanv");
@@ -31,16 +31,15 @@ setup = function() {
 
 draw = function(){
   //clear();
-  background('#fff');
+  /*background('#fff');
   if(maps.length > 0){
     for(var i = 0; i < maps[mapFocus].gridNodes.length; i++){
       maps[mapFocus].gridNodes[i][0] += random(-1,1);
       maps[mapFocus].gridNodes[i][1] += random(-1,1);
 
     }
-    maps[mapFocus].display();
+    maps[mapFocus].display();*/
     //console.log('ht');
-  }
 };
 
 gotFile = function(file) {
@@ -66,20 +65,18 @@ addMap = function(imgLoaded){
 };
 
 function mousePressed(){
-  for (var i = 0; i < maps[mapFocus].gridNodes.length; i++){
-    //console.log(dist(mouseX, mouseY, maps[mapFocus].gridNodes[0][0], maps[mapFocus].gridNodes[0][1]));
-    if (dist(mouseX+20, mouseY+20, maps[mapFocus].gridNodes[i][0], maps[mapFocus].gridNodes[i][1]) < 50){
-      dragging = true;
-      dragOffX =  maps[mapFocus].gridNodes[i][0] - mouseX;
-      dragOffY =  maps[mapFocus].gridNodes[i][1] - mouseY;
-      maps[mapFocus].dragging(i);
-    }
+  maps[mapFocus].dragLock();
+}
+
+function mouseDragged(){
+  if(dragging){
+    maps[mapFocus].dragging();
   }
 }
 
 function mouseReleased(){
   dragging = false;
-  maps[mapFocus].display();
+  maps[mapFocus].reDraw();
 }
 
 //map class, contains main data structure
@@ -88,11 +85,12 @@ function Map(name, opac, img, xoff, id){
 	this.opac = opac;
 	this.img = img;
 	this.id = id;
-	this.offSetX = 20 - canvasW/2; //WEBGL centers (0,0) on screen, canvasW/2 returns image to top left
-	this.offSetY = 20 - canvasH/2;
+	this.offSetX = 0 - canvasW/2; //WEBGL centers (0,0) on screen, canvasW/2 returns image to top left
+	this.offSetY = 0 - canvasH/2;
   this.gridNodes = [];
-  this.gridCols = 10;
-  this.gridRows = 10;
+  this.draggingNodes = [];
+  this.gridCols = 20;
+  this.gridRows = 20;
 
   this.makeNew = function(){
    //this.gridNodes = [];
@@ -120,18 +118,27 @@ function Map(name, opac, img, xoff, id){
 		//image(this.img,0,0,this.img.width,this.img.height);
     beginShape(TRIANGLES);
     stroke(100);
-    fill(255,50);
+    noFill();
 
     textureMode(NORMAL);
     texture(this.img);
     //console.log(this.gridNodes);
 
     //plots triangles
+    /*
+    for(var i = 0; i < this.gridNodes.length-2; i++){
+      vertex(this.gridNodes[i][0],this.gridNodes[i][1],this.gridNodes[i][2],this.gridNodes[i][3]);
+      vertex(this.gridNodes[i+1][0],this.gridNodes[i+1][1],this.gridNodes[i+1][2],this.gridNodes[i+1][3]);
+      vertex(this.gridNodes[i+2][0],this.gridNodes[i+2][1],this.gridNodes[i+2][2],this.gridNodes[i+2][3]);
+    }*/
+
+
     for (var x = 0; x < this.gridCols; x++){
       for (var y = 0; y < this.gridRows*2;y++){
         vertex(this.gridNodes[x*(this.gridRows*2+2)+y][0], this.gridNodes[x*(this.gridRows*2+2)+y][1], this.gridNodes[x*(this.gridRows*2+2)+y][2], this.gridNodes[x*(this.gridRows*2+2)+y][3]);
         vertex(this.gridNodes[x*(this.gridRows*2+2)+1+y][0], this.gridNodes[x*(this.gridRows*2+2)+1+y][1], this.gridNodes[x*(this.gridRows*2+2)+1+y][2], this.gridNodes[x*(this.gridRows*2+2)+1+y][3]);
         vertex(this.gridNodes[x*(this.gridRows*2+2)+2+y][0], this.gridNodes[x*(this.gridRows*2+2)+2+y][1], this.gridNodes[x*(this.gridRows*2+2)+2+y][2], this.gridNodes[x*(this.gridRows*2+2)+2+y][3]);
+        //console.log(x*(this.gridRows*2+2)+y);
       }
     }
 
@@ -144,9 +151,50 @@ function Map(name, opac, img, xoff, id){
     pop();
 	};
 
+  this.reDraw = function(){
+    push();
+		translate(this.offSetX,this.offSetY);
+    clear();
+    beginShape(TRIANGLES);
+    textureMode(NORMAL);
+    texture(this.img);
+    for (var x = 0; x < this.gridCols; x++){
+      for (var y = 0; y < this.gridRows*2;y++){
+        vertex(this.gridNodes[x*(this.gridRows*2+2)+y][0], this.gridNodes[x*(this.gridRows*2+2)+y][1], this.gridNodes[x*(this.gridRows*2+2)+y][2], this.gridNodes[x*(this.gridRows*2+2)+y][3]);
+        vertex(this.gridNodes[x*(this.gridRows*2+2)+1+y][0], this.gridNodes[x*(this.gridRows*2+2)+1+y][1], this.gridNodes[x*(this.gridRows*2+2)+1+y][2], this.gridNodes[x*(this.gridRows*2+2)+1+y][3]);
+        vertex(this.gridNodes[x*(this.gridRows*2+2)+2+y][0], this.gridNodes[x*(this.gridRows*2+2)+2+y][1], this.gridNodes[x*(this.gridRows*2+2)+2+y][2], this.gridNodes[x*(this.gridRows*2+2)+2+y][3]);
+        //console.log(x*(this.gridRows*2+2)+y);
+      }
+    }
+    endShape();
+
+    for (var i = 0; i < this.gridNodes.length;i++){
+      ellipse(this.gridNodes[i][0], this.gridNodes[i][1],5,5);
+    }
+    pop();
+  }
+
+  this.dragLock = function(){
+    //determines which nodes are locked for dragging and adds them to the draggingNodes array
+    this.draggingNodes = []; //clear array first
+
+    push();
+		translate(this.offSetX,this.offSetY);
+    for (var i = 0; i < this.gridNodes.length; i++){
+      if (dist(mouseX, mouseY, this.gridNodes[i][0], this.gridNodes[i][1]) < 10){
+        dragging = true;
+        this.draggingNodes.push([i,mouseX-this.gridNodes[i][0],mouseY-this.gridNodes[i][1]]); //[node#,diffX,diffY]
+      }
+    }
+    pop();
+  };
+
   this.dragging = function(nodeNo){
-    this.gridNodes[nodeNo][0] = dragOffX + mouseX;
-    this.gridNodes[nodeNo][1] = dragOffX + mouseY;
+    for(var i = 0; i < this.draggingNodes.length; i++){
+      this.gridNodes[this.draggingNodes[i][0]][0] = mouseX - this.draggingNodes[i][1];
+      this.gridNodes[this.draggingNodes[i][0]][1] = mouseY - this.draggingNodes[i][2];
+    }
+    this.reDraw();
   };
 
 };
